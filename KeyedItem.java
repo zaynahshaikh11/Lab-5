a5ffde80d6726a81f0a6481245a9dfca913b2516
Zaynah Shaikh <zaynahshaikh11@gmail.com>
2017-12-15 01:26:16 -0800

    Add files via upload

---
+++ KeyedItem.java
@@ -0,0 +1,13 @@
+package SearchKeys;
+public abstract class KeyedItem <KT extends Comparable <? super KT>>
+{
+	private KT searchKey;
+	public KeyedItem(KT key)
+	{
+		searchKey = key;
+	}
+	public KT getKey()
+	{
+		return searchKey;
+	}
+}
